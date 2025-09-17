c06c10c67b11f564ee4d4805364ce0cb672d3c4b
Jorge Emilio <jorgeemilio7777@gmail.com>
2025-09-17 14:41:13 -0700

    Se crearon nuevas pruebas

--- test/nombre_test.dart
+++ test/nombre_test.dart
@@ -1,9 +1,34 @@
 import 'package:test/test.dart';
 import 'package:batalla_naval/nombre.dart';
 
-void main(){
-  test('Debe tener un minimo de longitud', () {
-    expect(() => Nombre(""), throwsA(isA<LongitudMinimaException>()));
+void main() {
+  group('Validaciones de Nombre', () {
+    test('Debe tener un mínimo de longitud', () {
+      expect(() => Nombre("abc"), throwsA(isA<LongitudMinimaException>()));
+    });
 
+    test('Debe tener un máximo de longitud', () {
+      expect(() => Nombre("abcdefghijklmnop"), throwsA(isA<LongitudMaximaException>()));
+    });
+
+    test('No debe comenzar con un número', () {
+      expect(() => Nombre("1nombre"), throwsA(isA<PrimerCaracterNumeroException>()));
+    });
+
+    test('No debe tener caracteres especiales', () {
+      expect(() => Nombre("abc#de"), throwsA(isA<CaracteresInvalidosException>()));
+    });
+    
+    test('No debe haber espacios', () {
+      expect(() => Nombre("abc def"), throwsA(isA<CaracteresInvalidosException>()));
+    });
+
+    test('Debe aceptar un nombre válido', () {
+      expect(Nombre("Carlos").candidato, equals("Carlos"));
+    });
+
+    test('Debe aceptar nombre con números pero no al inicio', () {
+      expect(Nombre("Juan123").candidato, equals("Juan123"));
+    });
   });
 }
