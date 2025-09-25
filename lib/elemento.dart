8110c7cf524df7f6b86eb7620332aaebc267a0a0
Jorge Emilio <jorge.emilio.chavez.lizarraga@gmail.com>
2025-09-25 11:34:17 -0700

    Se implementaron los cambios de ayer y antier

---
+++ lib/elemento.dart
@@ -0,0 +1,9 @@
+import 'package:batalla_naval/punto.dart';
+
+class Elemento{
+
+  final Punto punto;
+  bool tocado = false;
+Elemento({required  this.punto});
+
+}
---
+++ lib/punto.dart
@@ -0,0 +1,21 @@
+import 'dart:math';
+
+class Punto {
+final int columna;
+final int fila;
+
+Punto({required this.columna, required this.fila});
+
+
+int distancia(Punto otro){
+int x = columna - otro.columna;
+int y = fila - otro.fila;
+
+num hipo = pow(x,2) + pow(y,2);
+num raiz = sqrt(hipo);
+return raiz.toInt();
+
+return 0;
+
+}
+}
